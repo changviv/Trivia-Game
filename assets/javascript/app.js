26fad887db40d9d93ecfbd4503c9aca6b5fdf8ed
changviv <vivien@easypost.com>
2018-10-18 17:00:02 -0700

    basic game structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,78 @@
-window.onload = function() {
-  $(".start").on("click", timer.start);
-};
+var timer;
 
-var timerRunning = false
+$("#start-page").on("click", function() {
+	game.start();
+});
 
-var timer = {
-	minutes: 2,
-	seconds:60,
-	reset: function() {
-		timer.minutes = 2;
-		timer.seconds = 60;
-		$("#display").text("2:00");
+$("#results-page").hide();
 
+var questions = [
+		{
+    	question: "What color is the sky?",
+    	choices: ["Blue", "Red", "Green", "Brown"],
+    	answer: "Blue"
+    },
+    {
+    	question: "Which one is not part of the Great Lakes?",
+    	choices: ["Erie", "Ontario", "Hudson", "Michigan"],
+    	answer: "Hudson"
+    },
+    {
+    	question: "Who is the Golde State Warrior's Coach?",
+    	choices: ["Gregg Popovich", "Luke Walton", "Kenny Atkinson", "Steve Kerr"],
+    	answer: "Steve Kerr"
+    },
+    {
+    	question: "Freddie Mercury died in which year?",
+    	choices: ["1990", "1991", "1992", "1993"],
+    	answer: "1991"
+    },
+    {
+    	question: "Who is the 2018 female recipient of the Nobel Prize in Physics?",
+    	choices: ["Donna Strickland", "Frances Arnold", "Marie Curie", "Malala Yousafzai"],
+    	answer: "Donna Strickland"
+    },    
+    {
+    	question: "Which is not Ralph Waldo Emerson's work?",
+    	choices: ["The American Scholar", "Nature", "Walden", "Self-Reliance"],
+    	answer: "Walden"
+    }
+];
+
+
+var game = {
+	correct: 0,
+	incorrect: 0,
+	unanswered: 0,
+	counter: 10,
+	countdown: function() {
+		game.counter--;
+		$("#show-timer").text(game.counter);
+		$(".start").hide()
+
+		if (game.counter === 0) {
+			game.stop();
+		};
 	},
 	start: function() {
-		timer.reset;
-		if (!timerRunning) {
-			intervalId = setInterval(timer.count, 1000);
-      timerRunning = true;
-    }
+		$("#start-page").hide();
+		timer = setInterval(game.countdown, 1000);
+		console.log("start");
+		for (var i = 0; i < questions.length; i++) {
+			$("#questions").append("<h2>" + (i + 1)+ ". " + questions[i].question + "</h2>");
+			$("#answers").append("<p>" + questions[i].choices + "</p>");
+		}
 	},
 	stop: function() {
-    clearInterval(intervalId);
-    clockRunning = false;
-  },
-	count: function() {
-		timer.seconds--;
-		var converted = timer.timeConverter(timer.minutes,timer.seconds);
-		$("#show-timer").text(converted);
+		console.log("stop");
+		clearInterval(timer);
+		$("#question-page").hide();
+		$("#results-page").show();
+		$("#no-answers").text(game.unanswered);
+		$("#correct-answers").text(game.correct);
+		$("#wrong-answers").text(game.incorrect);
 	},
-	timeConverter: function(min, sec) {
-		if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
+};
 
-    if (sec === 0) {
-			min--;
-			timer.seconds = 60;
-			timer.seconds--;
-		}
 
-		return minutes + ":" + seconds;
-	}
-}
+
