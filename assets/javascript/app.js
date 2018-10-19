fd022cbf0d683a0ad5b2c6e4d96c1d878cdce5e7
Vivien <vchang103@gmail.com>
2018-10-19 09:57:24 -0700

    edit script and html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,14 @@ $("#start-page").on("click", function() {
 	game.start();
 });
 
+$("#question-page").on("click", "#submit", function() {
+	game.results();
+	game.stop();
+	console.log("This has been submitted");
+});
+
+// play again button after result page is loaded
+
 $("#results-page").hide();
 
 var questions = [
@@ -31,7 +39,7 @@ var questions = [
     	question: "Who is the 2018 female recipient of the Nobel Prize in Physics?",
     	choices: ["Donna Strickland", "Frances Arnold", "Marie Curie", "Malala Yousafzai"],
     	answer: "Donna Strickland"
-    },    
+    },
     {
     	question: "Which is not Ralph Waldo Emerson's work?",
     	choices: ["The American Scholar", "Nature", "Walden", "Self-Reliance"],
@@ -44,7 +52,7 @@ var game = {
 	correct: 0,
 	incorrect: 0,
 	unanswered: 0,
-	counter: 10,
+	counter: 45,
 	countdown: function() {
 		game.counter--;
 		$("#show-timer").text(game.counter);
@@ -57,22 +65,60 @@ var game = {
 	start: function() {
 		$("#start-page").hide();
 		timer = setInterval(game.countdown, 1000);
-		console.log("start");
 		for (var i = 0; i < questions.length; i++) {
-			$("#questions").append("<h2>" + (i + 1)+ ". " + questions[i].question + "</h2>");
-			$("#answers").append("<p>" + questions[i].choices + "</p>");
-		}
+      $("#question-page").append('<h2>' + questions[i].question + '</h2>');
+      for (var j = 0; j < questions[i].choices.length; j++){
+        $("#question-page").append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
+      }
+  	}
+  	$("#question-page").append("<button id='submit'>Submit</button>");
+	},
+	results: function() {
+
+
+
+	// 	$("input[type=radio][checked]").each(function() {
+
+	// 	})
+		$.each(questions, function(index, value) {
+				if ( questions[index].answer === $('input[type=radio]:checked').val() ) {
+					game.correct++
+				} else {
+					game.incorrect++
+				}
+			console.log($('input[type=radio]:checked').val())
+		})
+
+	// 	game.stop();
 	},
 	stop: function() {
-		console.log("stop");
 		clearInterval(timer);
+		$("#time").hide();
 		$("#question-page").hide();
 		$("#results-page").show();
 		$("#no-answers").text(game.unanswered);
 		$("#correct-answers").text(game.correct);
 		$("#wrong-answers").text(game.incorrect);
+
 	},
+
 };
 
+function correctAnswer() {
+	correct++;
+}
+
+function wrongAnswer() {
+	wrong++;
+}
+
+function unanswered() {
+	unanswered++
+}
+
+function newQuestion() {
+
+}
 
 
+// calculate the score
