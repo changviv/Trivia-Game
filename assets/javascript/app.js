b394da6d6e8aef5064f3de818166b9c3476cb693
changviv <vivien@easypost.com>
2018-10-19 17:16:36 -0700

    complete trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,21 @@
 var timer;
-
+// start the game
 $("#start-page").on("click", function() {
 	game.start();
 });
-
+//clicks submit when answers all questions
 $("#question-page").on("click", "#submit", function() {
 	game.results();
 });
-
-$("#results-page").on("click", "#replay", function() {
-	game.start();
-});
-
-
+// hide results page when start
+$(".question-container").hide();
 $("#results-page").hide();
 
 var questions = [
 		{
-    	question: "What color is the sky?",
-    	choices: ["Blue", "Red", "Green", "Brown"],
-    	answer: "Blue"
+    	question: "What is the name for a decorative pillowcase?",
+    	choices: ["Sham", "Jon Hamm", "Baby Pram", "Pillow"],
+    	answer: "Sham"
     },
     {
     	question: "Which one is not part of the Great Lakes?",
@@ -32,20 +28,40 @@ var questions = [
     	answer: "Steve Kerr"
     },
     {
+    	question: "Dr. Seuss's Lorax character claims to specifically pseak for what part of nature?",
+    	choices: ["Bees", "Trees", "Seas", "Leaves"],
+    	answer: "Trees"
+    },
+    {
     	question: "Freddie Mercury died in which year?",
     	choices: ["1990", "1991", "1992", "1993"],
     	answer: "1991"
     },
     {
-    	question: "Who is the 2018 female recipient of the Nobel Prize in Physics?",
-    	choices: ["Donna Strickland", "Frances Arnold", "Marie Curie", "Malala Yousafzai"],
-    	answer: "Donna Strickland"
+    	question: "The Komodo dragon is the national animal of where?",
+    	choices: ["Indonesia", "Papua New Guinea", "Phillipines", "Cambodia"],
+    	answer: "Indonesia"
     },
     {
     	question: "Which is not Ralph Waldo Emerson's work?",
     	choices: ["The American Scholar", "Nature", "Walden", "Self-Reliance"],
     	answer: "Walden"
-    }
+    },
+    {
+    	question: "Which is the national flower of Monaco?",
+    	choices: ["Carnationr", "Bluebonnet", "Sunflowers", "Zinnia"],
+    	answer: "Zinnia"
+    },
+    {
+    	question: "Which American artist was known for his mobiles?",
+    	choices: ["Jackson Pollock", "Jasper Johns", "Alexander Calder", "Norman Rockwell"],
+    	answer: "Alexander Calder"
+    },
+    {
+    	question: "'And now that you don't have to be perfect, you can be good' is a quote from which Steinbeck novel?",
+    	choices: ["Of Mice and Men", "East of Eden", "The Grapes of Wrath", "No One Knows"],
+    	answer: "East of Eden"
+    },
 ];
 
 var game = {
@@ -57,44 +73,45 @@ var game = {
 		game.counter--;
 		$("#show-timer").text(game.counter);
 		$(".start").hide()
-
 		if (game.counter === 0) {
 			game.results();
 		};
 	},
 	start: function() {
 		$("#start-page").hide();
+		$(".question-container").show();
 		timer = setInterval(game.countdown, 1000);
 		for (var i = 0; i < questions.length; i++) {
-      $("#question-page").append('<h2>' + questions[i].question + '</h2>');
-      for (var j = 0; j < questions[i].choices.length; j++){
-        $("#question-page").append('<input type="radio" name ="question' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
-      }
-  	}
-  	$("#question-page").append("<button id='submit'>Submit</button>");
+	    $("#question-page").append('<h2>' + questions[i].question + '</h2>');
+	    for (var j = 0; j < questions[i].choices.length; j++){
+	      $("#question-page").append("<input type='radio' id='question" + i + "' value='" + questions[i].choices[j] + "'>","<label>" + questions[i].choices[j] + "</label>");
+	    }
+		}
+		$("#question-page").append("<br><button type='button' class='btn btn-light' id='submit'>Submit</button>");
 	},
 	results: function() {
-			for (var j = 0; j < questions.length; j++) {
-				if (!$("input[name='question" + j + "']").is(":checked")) {
-					game.unanswered++
-				}
-				else {
-					if ($("input[name='question" + j + "']:checked").val() === questions[j].answer) {
-						game.correct++
-					} else {
-						game.incorrect++
-					}
+		for (var j = 0; j < questions.length; j++) {
+			if (!$("input[id='question" + j + "']").is(":checked")) {
+				game.unanswered++
+			}
+			else {
+				if ($("input[id='question" + j + "']:checked").val() === questions[j].answer) {
+					game.correct++
+				} else {
+					game.incorrect++
 				}
 			}
-		game.stop()
+		}
+		game.stop();
 	},
 	stop: function() {
 		clearInterval(timer);
 		$("#time").hide();
 		$("#question-page").hide();
+		$(".question-container").hide();
 		$("#results-page").show();
 		$("#no-answers").text(game.unanswered);
 		$("#correct-answers").text(game.correct);
 		$("#wrong-answers").text(game.incorrect);
-	},
+	}
 };
